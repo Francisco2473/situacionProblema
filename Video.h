79e21cb1f65b557d75ba2e61ba46562e99c70b3b
Francisco Tonatihu <A01749518@tec.mx>
2023-06-14 12:47:26 -0600

    Clase Video

--- Video.h
+++ Video.h
@@ -1,18 +1,38 @@
 /*
 Autor: Francisco Tonatihu Castro Flores   A01749518
-Declaracion de clase padre Video 
+Declaracion de clase padre Video
 */
 
 #ifndef ENVIO_H
 #define ENVIO_H
+#include <string>
+using namespace std;
 
 class Video
 {
-private:
-    
+protected:
+    string id;
+    string nombre;
+    string genero;
+    int calificacion;
+    int duracion;
+    string fechaEstreno;
+
+    public: 
+    Video (const string id, const string nombre, const string genero, int calificacion, int duracion, int fechaEstreno);
+    string getId()const;
+    string getNombre()const;
+    string getGenero()const;
+    int getCalificacion()const;
+    int getDuracion()const;
+    string getFechaEstreno()const;
+
+
+    void setCalificacion(int alificacion);
+
+    virtual void mostrarInformacion()const=0;
 
 public:
-  
 };
 
 #endif
